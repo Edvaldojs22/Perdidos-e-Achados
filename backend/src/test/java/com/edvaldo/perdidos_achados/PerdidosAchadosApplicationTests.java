51ea5da819872f9a9a711f45529f3f36e7d824ea
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-10-27 20:49:48 -0300

    first commit

---
+++ backend/src/test/java/com/edvaldo/perdidos_achados/PerdidosAchadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edvaldo.perdidos_achados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerdidosAchadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
