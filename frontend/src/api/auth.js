599a28957fc1e392ad67c523d20b17b41bede5bf
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-11-03 22:18:54 -0300

    Adiciona implementação de autenticação: cria funções de login e gerenciamento de token, e configura axios para incluir o token nas requisições.

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,16 @@
+import api from "./axios";
+export const login = async (email, senha) => {
+  try {
+    const respose = await api.post("/auth/login", {
+      email,
+      senha,
+    });
+
+    const token = respose.data.token;
+    localStorage.setItem("token", token);
+    return respose.data;
+  } catch (error) {
+    console.log(error);
+    throw error.respose;
+  }
+};
