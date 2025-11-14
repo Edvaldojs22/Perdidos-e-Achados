5c7af52a618f6ac1a4c0b7df96eb3795323f36b3
Edvaldojs22 <jonas.edvaldo.10@gmail.com>
2025-11-14 17:55:13 -0300

    Refactor CORS configuration to use dynamic allowed origins from application properties

--- backend/src/main/java/com/edvaldo/perdidos_achados/security/cors/CoresConfig.java
+++ backend/src/main/java/com/edvaldo/perdidos_achados/security/cors/CoresConfig.java
@@ -1,5 +1,6 @@
 package com.edvaldo.perdidos_achados.security.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,13 +8,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CoresConfig {
+    @Value("${APP_CORS_ALLOWED_ORIGINS}")
+    private String[] allowedOrigins;
     @Bean
     public WebMvcConfigurer corsConfigurer(){
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry){
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173/")
+                        .allowedOrigins(allowedOrigins)
                         .allowedMethods("GET","POST","DELETE","OPTIONS","PUT")
                         .allowedHeaders("*")
                         .allowCredentials(true);
